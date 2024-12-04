c576773ea9645274af27eca6c35259d32581e90d
sonjel <sondanganjelina0@gmail.com>
2024-12-04 18:36:56 +0700

    ini versi 1

--- panen.py
+++ panen.py
@@ -1,1 +1,53 @@
-print("sondang")
+data_panen = {
+    'lokasi1':{
+        'nama_lokasi' : 'Kebun A',
+        'hasil_panen' : {
+            'padi' : 1200,
+            'jagung' : 800,
+            'keledai' : 500
+        }
+    },
+    'lokasi2' : {
+        'nama_lokasi' : 'Kebun B',
+        'hasil_panen' : {
+            'padi' : 1500,
+            'jagung' : 1000,
+            'keledai' : 500
+        }
+    },
+    'lokasi3' : {
+        'nama_lokasi' : 'Kebun C',
+        'hasil_panen' : {
+            'padi' : 1800,
+            'jagung' : 1200,
+            'keledai' : 600
+        }
+    },
+    'lokasi4' : {
+        'nama_lokasi' : 'Kebun D',
+        'hasil_panen' : {
+            'padi' : 2000,
+            'jagung' : 1500,
+            'keledai' : 700
+        }
+    },
+    'lokasi5' : {
+        'nama_lokasi' : 'Kebun E',
+        'hasil_panen' : {
+            'padi' : 2200,
+            'jagung' : 1800,
+            'keledai' : 800
+        }
+    },
+    'lokasi5' : {
+        'nama_lokasi' : 'Kebun F',
+        'hasil_panen' : {
+            'padi' : 2400,
+            'jagung' : 2000,
+            'keledai' : 900
+        }
+    }
+}
+
+# Ini adalah perubahan untuk branch Baru
+print("Branch Baru Aktif")
